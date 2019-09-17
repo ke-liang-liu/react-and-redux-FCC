563e7d6151a6584382237e219f0415e9cf498f2c
Keliang Liu <keliangliu1@gmail.com>
2019-09-18 03:03:46 +1000

    first commit

---
+++ src/index.js
@@ -0,0 +1,93 @@
+import React, {propTypes} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+import { createStore } from 'redux';
+// import Redux from 'redux';  //compile error: Attempted import error: 'redux' does not contain a default export (imported as 'Redux').
+
+
+import { Provider } from 'react-redux';
+
+
+// Redux Code:
+const ADD = 'ADD';
+
+const addMessage = (message) => {
+  return {
+    type: ADD,
+    message
+  }
+};
+
+const messageReducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD:
+      return [
+        ...state,
+        action.message
+      ];
+    default:
+      return state;
+  }
+};
+
+
+
+const store = createStore(messageReducer);
+
+// React Code:
+
+class DisplayMessages extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      messages: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  submitMessage() {
+    const currentMessage = this.state.input;
+    this.setState({
+      input: '',
+      messages: this.state.messages.concat(currentMessage)
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input
+          value={this.state.input}
+          onChange={this.handleChange}/><br/>
+        <button onClick={this.submitMessage}>Submit</button>
+        <ul>
+          {this.state.messages.map( (message, idx) => {
+              return (
+                 <li key={idx}>{message}</li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    );
+  }
+};
+
+// const Provider = ReactRedux.Provider;
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <DisplayMessages />
+  </Provider>,
+  document.getElementById('root')
+);
+
+// ReactDOMServer.renderToString(<App />);  // Render React on the Server
